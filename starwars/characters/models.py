c8d72d79d949701c95d829e45f7841b35a245da3
Miguel Mesa <mmesas369@gmail.com>
2021-06-08 15:07:35 -0500

    Creo los modelos Character, Movie, Planet

--- starwars/characters/models.py
+++ starwars/characters/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
+class Character(models.Model):
+    name = models.CharField(max_length=250, unique=True, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+    
+    def __repr__(self):
+        return self.name
+
+
+class Planet(models.Model):
+    name = models.CharField(max_length=250, unique=True, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    name = models.CharField(max_length=250, unique=True, null=False, blank=False)
+    year = models.PositiveSmallIntegerField(blank=True, null=True)
+    opening_text = models.TextField(max_length=2000, blank=False, null=False)
+    director_name = models.CharField(max_length=250, unique=True, null=False, blank=False)
+    planets = models.ManyToManyField(Planet)
+    characters = models.ManyToManyField(Character)
+
+    def __str__(self):
+        return self.name
+    
+    def __repr__(self):
+        return self.name
